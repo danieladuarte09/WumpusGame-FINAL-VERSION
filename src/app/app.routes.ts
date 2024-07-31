4d7198b354b6ef37e8b8b03448a67b7c4fef11df
danieladuarte09 <danielaa090799#gmail.com>
2024-07-31 10:41:50 +0200

    Configured box sizes and responsive design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,17 @@ import { Routes } from '@angular/router';
 
 
 export const routes: Routes = [
-    {path: 'gameboard', 
-    loadComponent: ()=> import('./Components/game-board/game-board.component').then(m => m.GameBoardComponent) ,
-},
-{path: 'formpage', 
-    loadComponent: ()=> import('./Components/form-page/form-page.component').then(m => m.FormPageComponent) }
+    { 
+        path: '', 
+        redirectTo: 'formpage', 
+        pathMatch: 'full' 
+    },
+    { 
+        path: 'gameboard', 
+        loadComponent: () => import('./Components/game-board/game-board.component').then(m => m.GameBoardComponent)
+    },
+    { 
+        path: 'formpage', 
+        loadComponent: () => import('./Components/form-page/form-page.component').then(m => m.FormPageComponent)
+    }
 ];
